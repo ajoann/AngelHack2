26ecc7d12688c6d1b6182a84b5f1d3e105817546
Amanda <ajhwhiz@gmail.com>
2017-07-30 11:20:23 -0700

    updated with React and graphs

--- test/Title.js
+++ test/Title.js
@@ -7,7 +7,7 @@ import Title from '../frontend/components/Title';
 describe('Component: App', () => {
   it('should render the App component', () => {
     const wrapper = shallow(
-      <Title name="Horizons" />
+      <Title name="TRC" />
     );
 
     expect(wrapper.find('h1').text()).toEqual('Horizons');
